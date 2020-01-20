58efd77fb108c9f5127c65d51fc1ad341e903a05
ville <ville@DESKTOP-40G5A3U.home>
2020-01-20 13:33:30 +0200

    Working

--- Ohjelmointi1/src/viikko1/perusteet/th/Juhlat.java
+++ Ohjelmointi1/src/viikko1/perusteet/th/Juhlat.java
@@ -1,22 +1,22 @@
 package viikko1.perusteet.th;
 
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Juhlat {
-	
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Anna juhliin tulevien aikuisten määrä: ");
 		double aikuistenMäärä = scanner.nextDouble();
-		int pullo = 7;
+		double pullo = 7;
 		double kappaleMäärä = Math.ceil(aikuistenMäärä / pullo);
-		DecimalFormat df = new DecimalFormat ("0");
-		System.out.println("Pulloja tarvitaan "+ df.format(kappaleMäärä) + " kappaletta");
-		double ylimääräinen = Math.ceil(aikuistenMäärä%pullo);
-		System.out.println("Viimeiesestä pullosta jää " + df.format(ylimääräinen) + " lasia");
-		
+		DecimalFormat df = new DecimalFormat("0");
+		System.out.println("Pulloja tarvitaan " + df.format(kappaleMäärä) + " kappaletta");
+		double ylimääräinen = pullo - Math.ceil(aikuistenMäärä % pullo);
+		System.out.println("Viimeisestä pullosta jää " + df.format(ylimääräinen) + " lasia");
+
 	}
 
 }
-
