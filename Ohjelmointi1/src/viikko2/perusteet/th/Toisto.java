a8298652b35589abbd0e1cd6084a05a4dadbfc4e
vsihvola <ville.sihvola8@gmail.com>
2020-01-27 13:25:56 +0200

    Week 2 Day 1

---
+++ Ohjelmointi1/src/viikko2/perusteet/th/Toisto.java
@@ -0,0 +1,14 @@
+package viikko2.perusteet.th;
+
+public class Toisto {
+
+	public static void main(String[] args) {
+
+		int number = 100;
+
+		for (int i = 1; i <= number; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
