6ec8ba9e9d42439172f61c2cab32d457cd5f2fc4
vsihvola <ville.sihvola8@gmail.com>
2020-03-09 18:16:16 +0200

    Final

--- Ohjelmointi1/src/viikko7/perusteet/th/WordCount.java
+++ Ohjelmointi1/src/viikko7/perusteet/th/WordCount.java
@@ -17,9 +17,16 @@ public class WordCount {
 			List<String> list = Files.readAllLines(path, StandardCharsets.UTF_8);
 
 			int lineCount = list.size();
-			int wordCount = list.toString().split(" ").length;
-			String string = list.toString().replace("[", "").replace("]", "");
-			int markCount = string.length();
+			int wordCount = list.toString().split("\\s+").length;
+
+			int markCount = 0;
+			for (int i = 0; i < list.size(); i++) {
+				markCount += list.get(i).length();
+				if (list.get(i).length() < 1) {
+					lineCount--;
+				}
+			}
+
 			System.out.println("Tiedostossa on: ");
 
 			System.out.println(lineCount + " riviä");
